47d05b446e03fe4ff1caee6c31ec20fc9e76626d
marcielfelipe <marciel.felipe@outlook.com>
2025-02-27 17:34:00 -0300

    feat: add first post

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,13 +23,18 @@ export default function RootLayout({ children }: RootLayoutProps) {
         className={`antialiased min-h-screen bg-white dark:bg-slate-950 text-slate-900 dark:text-slate-50 ${inter.className}`}
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <div className="max-w-2xl mx-auto py-10 px-4">
+          <div className="max-w-2xl mx-auto pb-10 px-4">
             <header>
+              <div className='pb-5'>
+                <h1 className='text-xl'>
+                  fordev<span className='text-fuchsia-800 text-5xl'>.</span><strong>dev</strong>
+                </h1>
+              </div>
               <div className="flex items-center justify-between">
                 <ModeToggle />
                 <nav className="ml-auto text-sm font-medium space-x-6">
                   <Link href="/">Home</Link>
-                  <Link href="/about">About</Link>
+                  {/* <Link href="/about">About</Link> */}
                 </nav>
               </div>
             </header>
