476afd4ca82a302bebbf8ff9fe4969306e2da483
marcielfelipe <marciel.felipe@outlook.com>
2025-03-04 17:23:38 -0300

    log tagmanager

--- components/google-tag-manager.tsx
+++ components/google-tag-manager.tsx
@@ -2,7 +2,7 @@
 import { GoogleTagManager } from '@next/third-parties/google'
 
 export default function GoogleTM() {
-  
+  console.log(process.env.NEXT_PUBLIC_GTM_ID)
   if(!process.env.NEXT_PUBLIC_GTM_ID)return null
   return (
     <>
